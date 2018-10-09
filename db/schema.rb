c6365367aff8d25bb937a7ba1693fa03529d5036
Backyardcrypto <alex.scagz@backyardcrypto.com>
2018-10-09 18:21:29 -0400

    deploying to heroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_09_184303) do
+ActiveRecord::Schema.define(version: 2018_10_09_201151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2018_10_09_184303) do
     t.datetime "updated_at", null: false
     t.integer "quantity", default: 1
     t.bigint "order_id"
+    t.decimal "price"
     t.index ["cart_id"], name: "index_line_items_on_cart_id"
     t.index ["order_id"], name: "index_line_items_on_order_id"
     t.index ["product_id"], name: "index_line_items_on_product_id"
